0084e8c973b04fcddd00523567a465e9aac0ae5d
laimoh <laiqamohid73@gmail.com>
2021-05-20 08:30:43 +0100

    get port from env variable

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ let stop = JSON.parse(stopdata);
 let app = express()
 app.use(express.static('public'))
 app.use(express.json())
-let port = 3000
+
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {
    console.log(`Listening on port ${port}.`)
